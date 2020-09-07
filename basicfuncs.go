20d4fde8e394fd3ff3bec87f18c69c560c26b775
machinexa <machinexa@gmail.com>
2020-09-07 18:42:06 +0545

    06:42:06 PM 09/07/2020

--- basicfuncs.go
+++ basicfuncs.go
@@ -3,7 +3,7 @@ package gobasic
 import "fmt"
 import "net/url"
 
-func urldecode(encoded_string string) string {
+func Urldecode(encoded_string string) string {
 	decoded_string, err := url.QueryUnescape(encoded_string);
 	ehandler(err);
 	decoded_string, err = url.QueryUnescape(decoded_string);
@@ -11,7 +11,7 @@ func urldecode(encoded_string string) string {
 	return decoded_string;
 }
 
-func ehandler(err error){
+func Ehandler(err error){
 	if err != nil {
 		fmt.Println(err);
 	}
---
+++ shit.go
@@ -0,0 +1,7 @@
+package main
+import "github.com/machinexa2/gobasic"
+
+func main() {
+	url := '%2Ahi%2A'
+	gobasic.urldecode(url);
+}
