5fc8f1613f19efd7cc241dd923373e5d30b253eb
root <machinexa>
2020-11-18 20:39:10 +0545

    08:39:10 PM 11/18/2020

--- gobasic.go
+++ gobasic.go
@@ -4,15 +4,8 @@ import "bufio"
 import "fmt"
 import "os"
 import "strings"
-//import "net/url"
 import "github.com/chzyer/readline"
-/*
-func Urldecode(encoded_string string) string {
-	decoded_string, _ := url.QueryUnescape(encoded_string);
-	decoded_string, _ = url.QueryUnescape(decoded_string);
-	return decoded_string;
-}
-*/
+
 func InputRead() string {
 	reader := bufio.NewReader(os.Stdin);
 	fmt.Print("> ");
