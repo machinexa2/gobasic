f8b15b06ee56603a87c8c6efa116140d843d5251
root <machinexa>
2020-11-18 21:08:37 +0545

    09:08:37 PM 11/18/2020

--- README.md
+++ README.md
@@ -3,7 +3,5 @@ Basic Go Functions: github.com/machinexa2/gobasic
 
 # Main: gobasic
 * InArray(check string, list string) bool: Python3 `x in iterable` alternative
-
-## InputFunctions: gobasic/cmd/input
 * Input(prompt string, preinput string) string: Python3 `input()` alternative
 
--- gobasic.go
+++ gobasic.go
@@ -1,5 +1,7 @@
 package gobasic
 
+import "github.com/chzyer/readline"
+
 func InArray(check string, list []string) bool {
         for _, individual := range list {
                 if individual == check {
@@ -9,3 +11,11 @@ func InArray(check string, list []string) bool {
         return false
 }
 
+
+func Input(prompt string, preinput string) string {
+        stdin, _ := readline.New(prompt);
+        defer stdin.Close();
+        stdin.WriteStdin([]byte(preinput));
+        value, _ := stdin.Readline()
+        return value
+}
