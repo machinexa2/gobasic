8e9edc6d4b6e7ab93296494e91b8ef3436e1e1c2
root <machinexa>
2020-11-22 10:48:55 +0545

    10:48:55 AM 11/22/2020

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Go basic
 Basic Go Functions: github.com/machinexa2/gobasic
 
-# Main: gobasic
+# Package: gobasic, github.com/machinexa2/gobasic
 * InArray(check string, list string) bool: Python3 `x in iterable` alternative
 * Input(prompt string, preinput string) string: Python3 `input()` alternative
 * ReadFile(filename string, c chan string) : Simple file reading
---
+++ pathfuncs/pathfuncs.go
@@ -0,0 +1,24 @@
+package pathfunctions
+
+func urler(xpath string) string{
+	if !(strings.HasPrefix(xpath, "http://") || strings.HasPrefix(xpath, "https://")){
+		return "http://" + xpath
+	}
+	return xpath
+}
+
+func ender(xpath string, ypath string) string{
+	if xpath[len(xpath)-1:] != ypath{
+		xpath += ypath
+	}
+	return xpath
+}
+
+func unstarter(xpath string, ypath string) string{
+	if xpath[:1] == ypath{
+		return xpath[1:]
+	}
+	return xpath
+}
+
+
